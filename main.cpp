412c7dc1a765df2fd3627857d6e0b915af3e4da2
JosephRArsany <jrsany2903@gmail.com>
2025-03-31 22:19:04 +0100

    Cannot find eigen file

--- main.cpp
+++ main.cpp
@@ -2,20 +2,22 @@
 #include <vector>
 #include <Eigen/Dense>
 
+
 using namespace std;
-using namespace Eigen;
+
+//using namespace Eigen;
 
 
 int main() {
-    Matrix <float, 3, 3> mat;
+    //Matrix <float, 3, 3> mat;
 
-
+/*
     for (int i = 0; i < mat.rows(); ++i) {
         for (int j = 0; j < mat.cols(); ++j) {
             std::cout << mat(i, j) << " ";
         }
         std::cout << std::endl;
-    }
-
+    }*/
+    cout << "Hello World!" << endl;
     return 0;
 }
